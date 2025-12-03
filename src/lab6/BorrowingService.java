07e439d6a95c84e5af663305c05fc74bc2a6c125
JennaLettuescode <113070970+JennaLettuescode@users.noreply.github.com>
2025-12-02 17:52:37 -0800

    Update BorrowingService.java
    Part 6: task 3: Update the BorrowingService class: Implement the borrowBook and returnBook methods to 
    return a BorrowingBookResult with both a success flag and a message indicating the result.

--- src/lab6/BorrowingService.java
+++ src/lab6/BorrowingService.java
@@ -4,30 +4,49 @@ public class BorrowingService implements BorrowingServiceAPI {
 	// DONE: Implement methods for borrowing
 	// and returning Books
 	@Override
-	public boolean borrowBook(Member member, Book book) {
-		if(member.getBorrowedBooks().contains(book)) {
-			return false;
+	public BorrowingBookResult borrowBook(Member member, Book book) {
+		BorrowingBookResult result = new BorrowingBookResult();
+			
+		if(member.getBorrowedBooks().contains(book)) { //The book is already borrowed by the same member 
+			result.setSuccess(false);
+			result.setBorrowingMessage("Member has already borrowed this book, cannot be borrowed");
+			return result;
 		}
-		if(book != null && book.getIsAvailable()) {
+		if(book != null && book.getIsAvailable()) { //The book is available
 			member.getBorrowedBooks().add(book);
 			book.setIsAvailable(false);
-			return true;
+
+			result.setSuccess(true);
+			result.setBorrowingMessage("The member has borrowed the book successfully");
+			return result;
 		}else {
-			return false;
-		}
+			result.setSuccess(false);
+			result.setBorrowingMessage("The member has borrowed unsuccessfully");
+			return result;
+		}	
 	}
 
 	@Override
-	public boolean returnBook(Member member, lab6.Book book) {
-		if(book == null) {
-			return false;
+	public BorrowingBookResult returnBook(Member member, lab6.Book book) {
+		BorrowingBookResult result = new BorrowingBookResult();
+		
+		if(book == null) { //The book dosen't exist
+			result.setSuccess(false);
+			result.setBorrowingMessage("Book dosen't exist, borrowing unsuccessful");
+			return result;
 		}
-		if(!member.getBorrowedBooks().contains(book)) {
-			return false;
+		if(!member.getBorrowedBooks().contains(book)) { //The member has not borrowed the book
+			result.setSuccess(false);
+			result.setBorrowingMessage("The member has not borrowed the book, borrowing unsuccessful");
+			return result;
 		}
 
 		member.getBorrowedBooks().remove(book);
 		book.setIsAvailable(true);
-		return true;
+		
+		result.setSuccess(true);
+		result.setBorrowingMessage("Book has been successfully returned");
+		return result;
 	}
 }
+
