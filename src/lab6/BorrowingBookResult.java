173293344bf42c6bd00ddf9e6ac5698e53389e0a
JennaLettuescode <113070970+JennaLettuescode@users.noreply.github.com>
2025-12-02 19:26:11 -0800

    Part 6: task 1

---
+++ src/lab6/BorrowingBookResult.java
@@ -0,0 +1,27 @@
+  package cosc236Lab6;
+//Part 6
+public class BorrowingBookResult {
+	
+	private boolean isSuccess;
+	private String borrowingMessage;
+	
+	//constructor taken directly from task 3
+	public BorrowingBookResult(boolean isSuccess, String borrowingMessage){
+		this.isSuccess = isSuccess; 
+		this.borrowingMessage = borrowingMessage; 
+	} 
+	
+	public void setSuccess(boolean isSuccess) {
+        this.isSuccess = isSuccess;
+    }
+	public boolean getSuccess() {
+        return isSuccess;
+    }
+
+	public void setBorrowingMessage(String borrowingMessage) {
+        this.borrowingMessage = borrowingMessage;
+    }
+	public String getBorrowingMessage() {
+        return borrowingMessage;
+    }
+}
